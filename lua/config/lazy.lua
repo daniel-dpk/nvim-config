27cd6750a7820ecf3652befc8ab85916aafe0605
Daniel Pook-Kolb <daniel.dpk@gmail.com>
2025-10-02 20:05:00 +0200

    Extract color settings to plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,19 +20,6 @@ vim.opt.runtimepath:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- [[ Colorscheme ]]
-    --{ "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme("tokyonight") end },
-    --{
-    --  "shaunsingh/nord.nvim", config = function()
-    --    vim.g.nord_contrast = true
-    --    vim.g.nord_borders = true
-    --    --vim.g.nord_cursorline_transparent = true
-    --    vim.cmd.colorscheme("nord")
-    --  end
-    --},
-    --{ "ramojus/mellifluous.nvim", config = function() vim.cmd.colorscheme("mellifluous") end },
-    { "HoNamDuong/hybrid.nvim", config = function() vim.cmd.colorscheme("hybrid") end },
-
     -- import all plugins from config/plugins/*
     { import = "config.plugins" },
   },
@@ -44,5 +31,3 @@ require("lazy").setup({
     enabled = false,
   },
 })
-
--- vim: ts=2 sts=2 sw=2 et
---
+++ lua/config/plugins/colors.lua
@@ -0,0 +1,13 @@
+return {
+    --{ "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme("tokyonight") end },
+    --{
+    --  "shaunsingh/nord.nvim", config = function()
+    --    vim.g.nord_contrast = true
+    --    vim.g.nord_borders = true
+    --    --vim.g.nord_cursorline_transparent = true
+    --    vim.cmd.colorscheme("nord")
+    --  end
+    --},
+    { "ramojus/mellifluous.nvim", config = function() vim.cmd.colorscheme("mellifluous") end },
+    --{ "HoNamDuong/hybrid.nvim", config = function() vim.cmd.colorscheme("hybrid") end },
+}
