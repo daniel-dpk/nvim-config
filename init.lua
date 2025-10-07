eba004ef4d4c790c1c2aa231530ee58a679786fc
Daniel Pook-Kolb <daniel.dpk@gmail.com>
2025-10-07 10:35:05 +0200

    Move keymaps.lua and options.lua to config

--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@ vim.g.maplocalleader = ','
 vim.g.have_nerd_font = true
 
 require('config.lazy')
-require('options')
-require('keymaps')
+require('config.options')
+require('config.keymaps')
 
 -- Load untracked extra configuration in, e.g.,
 --  ./lua/custom/init.lua
