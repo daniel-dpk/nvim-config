8533c2e138687da3b4ea2bc44059a1ef1328a99c
Daniel Pook-Kolb <daniel.dpk@gmail.com>
2025-10-07 19:36:30 +0200

    Fix opencode ask input again

--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -14,6 +14,13 @@ return {
         expand = false,
         win = {
           width = 120,
+          bo = {
+            textwidth = 0, -- auto-wrapping makes it unusable
+          },
+          keys = {
+            i_ctrl_e = { '<c-e>', '<End>', mode = "i", expr = true },
+            i_ctrl_a = { '<c-a>', '<Home>', mode = "i", expr = true },
+          },
         },
       },
     },
