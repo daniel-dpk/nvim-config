b3bd26d8e7cd836ab2092e4cab629f1afc8f6116
Daniel Pook-Kolb <daniel.dpk@gmail.com>
2025-10-07 19:49:54 +0200

    Fix diagnostic messages in treesitter.lua

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
         ensure_installed = {
@@ -31,7 +32,7 @@ return {
           --disable = { "c", "rust" },
 
           -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-          disable = function(lang, buf)
+          disable = function(_, buf)
               local max_filesize = 100 * 1024 -- 100 KB
               local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
               if ok and stats and stats.size > max_filesize then
