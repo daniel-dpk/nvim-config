658bf28f3fceb4e3ee022713452d4eb61760248d
Daniel Pook-Kolb <daniel.dpk@gmail.com>
2025-10-24 09:56:20 +0200

    Silence diagnostic

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -39,11 +39,12 @@ return {
 
           -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
           disable = function(_, buf)
-              local max_filesize = 100 * 1024 -- 100 KB
-              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-              if ok and stats and stats.size > max_filesize then
-                  return true
-              end
+            local max_filesize = 100 * 1024   -- 100 KB
+            ---@diagnostic disable-next-line: undefined-field
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
           end,
 
           -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
