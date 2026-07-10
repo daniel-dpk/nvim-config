c27eb336f0ed4f7c3e145154ab8e18d4a90d0394
Daniel Pook-Kolb <daniel.dpk@gmail.com>
2026-07-10 12:56:31 +0200

    Remove cwd-relative buffer names
    - Stop loading the buffer name normalizer
    - Delete the experimental helper
    
    Main issue with this approach is that NeoVim gets confused about which
    files and buffers are in sync, so you may randomly get prompts to confirm
    overwriting existing files during regular saves (mainly after creating new
    files).

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ vim.g.have_nerd_font = true
 
 require('config.lazy')
 require('config.options')
-require('utils.buffer_names').setup()
 require('config.keymaps')
 
 -- Load untracked extra configuration in, e.g.,
